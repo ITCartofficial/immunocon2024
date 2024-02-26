ce1fa236c4f24c763cae12bfc38f83488082a793
rezwanhussain <rezwanhussain80@gmail.com>
2024-02-26 16:06:44 +0530

    Updated national faculty details.

--- scripts/national_faculty.js
+++ scripts/national_faculty.js
@@ -1,292 +1,304 @@
 let nationalFaculty = [
   {
     name: "Amit Awasthi",
-    designation: "Managing Trustee",
-    description: "THSTI",
+    designation: "Senior Professor",
+    description: "Translational Health Science and Technology Institute (THSTI)",
     img: "../assets/img/national-faculty/Amit Awasthi.png"
   },
   {
     name: "Annapurna Vyakarnum",
-    designation: "Managing Trustee",
-    description: "St. John's Bangalore",
+    designation: "Professor",
+    description: "Microbial Immunology, St. John's Bangalore",
     img: "../assets/img/national-faculty/Annapurna Vyakarnam.png"
   },
   {
+    name: "Apurva Sarin",
+    designation: "CEO",
+    description: "DBT/Wellcome Trust India Alliance",
+    img: "../assets/img/national-faculty/Apurva Sarin.png"
+  },
+  {
     name: "Arunika Mukhopadhyay",
-    designation: "Managing Trustee",
-    description: "IISER Mohali",
+    designation: "Assistant Professor",
+    description: "Indian Institute of Science Education and Research (IISER) Mohali",
     img: "../assets/img/national-faculty/Arnuika Mukkhopadhyay IISER Mohali.png"
   },
   {
     name: "Devindra Sehgal",
-    designation: "Managing Trustee",
-    description: "NII",
+    designation: "Associate Professor",
+    description: "National Institute of Immunology (NII)",
     img: "../assets/img/national-faculty/Dr. Devinder Sehgal , NII.png"
   },
   {
     name: "Dhiraj Kumar",
-    designation: "Managing Trustee",
-    description: "ICGEB",
+    designation: "Group Leader ",
+    description: "Cellular Immunology,International Centre for Genetic Engineering and Biotechnology (ICGEB)",
     img: "../assets/img/national-faculty/Dhiraj Kumar , ICGEB.png"
   },
   {
     name: "Geeta Rai",
-    designation: "Managing Trustee",
-    description: "BHU",
+    designation: "Associate Professor",
+    description: "Dept of Molecular and Human Genetics, Institute of Science, Banaras Hindu University",
     img: "../assets/img/national-faculty/Dr. Geeta Rai, BHU.png"
   },
   {
     name: "Girdhari Lal",
-    designation: "Managing Trustee",
-    description: "NCCS",
+    designation: "Scientist",
+    description: "National Centre for Cell Science (NCCS)",
     img: "../assets/img/national-faculty/Dr. Girdhari Lal, NCCS.png"
   },
   {
     name: "Gurushankara",
-    designation: "Managing Trustee",
-    description: "Central University Kerala",
+    designation: "Assistant Professor",
+    description: "Department of Zoology, Central University Kerala",
     img: "../assets/img/national-faculty/Gurushankara , Central Univ Kerala.png"
   },
   {
     name: "Jyothi Prasanna",
-    designation: "Managing Trustee",
-    description: "Manipal Inst regenrative Med",
+    designation: "Professor",
+    description: "Manipal Institute of Regenerative Medicine, Bangalore",
     img: "../assets/img/national-faculty/Jyothi Prasanna , Manipal Inst regenrative Med.png"
   },
   {
     name: "Kalpana Luthra",
-    designation: "Managing Trustee",
-    description: "AIIMS Delhi",
+    designation: "Professor & Head",
+    description: "Department of Biochemistry, AIIMS New Delhi",
     img: "../assets/img/national-faculty/Kalpana Luthra Aiims.png"
   },
   {
     name: "Kesav Sannula",
-    designation: "Managing Trustee",
-    description: "IISc",
+    designation: "Assistant Professor",
+    description: "Indian Institute of Science (IISc), Bangalore",
     img: "../assets/img/national-faculty/Dr. Kesavardhana Sannula, IISc.png"
   },
   {
     name: "Krishna Mohan Atmakuri",
-    designation: "Managing Trustee",
-    description: "THSTI",
+    designation: "Associate Professor",
+    description: "Translational Health Science and Technology Institute (THSTI)",
     img: "../assets/img/national-faculty/Krishna Mohan Atmakuri , Thsti.png"
   },
   {
     name: "Kushagra Bansal",
-    designation: "Managing Trustee",
-    description: "JNCASR",
+    designation: "Professor",
+    description: "Jawaharlal Nehru Centre for Advanced Scientific Research (JNCASR)",
     img: "../assets/img/national-faculty/Dr. Kushagra Bansal, JNCASR.png"
   },
   {
     name: "M Madkaikar",
-    designation: "Managing Trustee",
-    description: "ICMR",
+    designation: "Deputy Director",
+    description: "Department of Paediatric Immunology and Leukocyte Biology, ICMR",
     img: "../assets/img/national-faculty/Manisha Madkaikar, ICMR.png"
   },
   {
     name: "M Netravathi",
-    designation: "Managing Trustee",
-    description: "NIMHANS",
+    designation: "Professor",
+    description: "Department of Neurology, NIMHANS",
     img: "../assets/img/national-faculty/M Netravathi NIMHANS.png"
   },
   {
     name: "Manzoor Mir",
-    designation: "Managing Trustee",
-    description: "University of Kashmir",
+    designation: "Head",
+    description: "Department of Bioresources at University of Kashmir",
     img: "../assets/img/national-faculty/Manzoor Mir , University of Kashmir.png"
   },
   {
     name: "Nagasuma Chandra",
-    designation: "Managing Trustee",
-    description: "IISc",
+    designation: "Professor",
+    description: "Department of Biochemistry, IISc",
     img: "../assets/img/national-faculty/Nagasuma Chandra, IISc.png"
   },
   {
     name: "Nimesh Gupta",
-    designation: "Managing Trustee",
-    description: "NII Delhi",
+    designation: "Principal Investigator ",
+    description: "National Institute of Immunology (NII) Delhi",
     img: "../assets/img/national-faculty/Dr. Nimesh Gupta , NII.png"
   },
   {
     name: "Pragya Yadav",
-    designation: "Managing Trustee",
-    description: "NIV Pune",
+    designation: "Scientist",
+    description: "National Institute of Virology (NIV) Pune",
     img: "../assets/img/national-faculty/Pragya Yadav NIV-Pune.png"
   },
   {
     name: "Rahul Purwar",
-    designation: "Managing Trustee",
-    description: "IIT-Bombay",
+    designation: "Associate Professor",
+    description: "Indian Institute of Technology (IIT) Bombay",
     img: "../assets/img/national-faculty/Rahul Purwar ,IIT-Bombay.png"
   },
   {
     name: "Rajiv Kumar",
-    designation: "Managing Trustee",
-    description: "BHU",
+    designation: "Assistant Professor ",
+    description: "Banaras Hindu University (BHU)",
     img: "../assets/img/national-faculty/Rajiv Kumar, BHU.png"
   },
   {
     name: "Rajeev Tyagi",
-    designation: "Managing Trustee",
-    description: "IMTECH",
+    designation: "Scientific Researcher",
+    description: "Institute of Microbial Technology (IMTECH)",
     img: "../assets/img/national-faculty/Rajeev Tyagi , IMTECH.png"
   },
   {
     name: "Ritobrata Goswami",
-    designation: "Managing Trustee",
-    description: "IIT Kanpur",
+    designation: "Associate Professor",
+    description: " Indian Institute of Technology (IIT) Kharagpur",
     img: "../assets/img/national-faculty/Ritobrata Goswami , IIT-Kanpur.png"
   },
   {
     name: "Rupesh Srivatsa",
-    designation: "Managing Trustee",
-    description: "AIIMS Delhi",
+    designation: "Assiocate Professor",
+    description: "All India Institute of Medical Sciences (AIIMS), New Delhi",
     img: "../assets/img/national-faculty/Rupesh Srivatsa , AIIMS Delhi.png"
   },
   {
-    name: "S Chandrasekhara",
-    designation: "Managing Trustee",
-    description: "ChangRe",
-    img: "../assets/img/national-faculty/S Chandrasekhara ,  ChangRe.png"
+    name: "Dr. S. Chandrashekara",
+    designation: "Rheumatologist",
+    description: "Clinical Immunology & Rheumatology, ChanRe Rheumatology and Immunology Center and Research",
+    img: "../assets/img/national-faculty/Dr. S. Chandrashekara, ChanRe.png"
   },
   {
     name: "Sachin Kumar",
-    designation: "Managing Trustee",
-    description: "IIT Guwahati",
+    designation: "Professor",
+    description: "Department of Biosciences & Bioengineering, IIT Guwahati",
     img: "../assets/img/national-faculty/Sachin Kumar, IIT Guwahati.png"
   },
   {
     name: "Sandeep Eswarappa",
-    designation: "Managing Trustee",
-    description: "IISc", 
+    designation: "Associate Professor",
+    description: "Department of Biochemistry, IISc Bangalore", 
     img: "../assets/img/national-faculty/Dr. Sandeep M Eswarappa, IISc.png"
   },
   {
     name: "Sandhya Ganesan",
-    designation: "Managing Trustee",
-    description: "IISER Trivandrum",
+    designation: "Assistant Professor",
+    description: "Indian Institute of Science Education and Research (IISER) Trivandrum",
     img: "../assets/img/national-faculty/Sandhya Ganesan , IISER Trivandrum.png"
   },
   {
     name: "Santosh Kumar",
-    designation: "Managing Trustee",
-    description: "CCMB",
+    designation: "Senior Scientist",
+    description: "Centre for Cellular & Molecular Biology (CCMB)",
     img: "../assets/img/national-faculty/Santosh Kumar, CCMB.png"
   },
   {
     name: "Shailiza Singh",
-    designation: "Managing Trustee",
-    description: "NCCS",
+    designation: "Scientist F",
+    description: "National Centre For Cell Science (NCCS)",
     img: "../assets/img/national-faculty/Shailiza Singh, NCCS.png"
   },
   {
     name: "Shashank Tripati",
-    designation: "Managing Trustee",
-    description: "IISc",
+    designation: "Assistant Professor",
+    description: "Department of Microbiology & Cell Biology, IISc",
     img: "../assets/img/national-faculty/Shashank Tripati iisc.png"
   },
   {
     name: "Shibnath Majumdar",
-    designation: "Managing Trustee",
+    designation: "Professor",
     description: "South Asian University",
     img: "../assets/img/national-faculty/Shibnath Majumdar, South Asian University.png"
   },
   {
     name: "Shilpak Chatterjee",
-    designation: "Managing Trustee",
-    description: "IICB",
+    designation: "Senior Scientist",
+    description: "Indian Institute of Chemical Biology (IICB)",
     img: "../assets/img/national-faculty/Shilpak Chatterjee , IICB.png"
   },
   {
     name: "Shiv Balasubramanian",
-    designation: "Managing Trustee",
+    designation: "Co-Founder & CEO",
     description: "Immunitas Labs",
     img: "../assets/img/national-faculty/Shiv Balasubramanian , Immunitas Labs.png"
   },
   {
     name: "Siddharth Jhunjhunwala",
-    designation: "Managing Trustee",
-    description: "IISc",
+    designation: "Associate Professor",
+    description: "Department of Bioengineering, IISc",
     img: "../assets/img/national-faculty/Dr. Siddharth Jhunjhunwala, IISc.png"
   },
   {
     name: "Siva Vallabhpurapu",
-    designation: "Managing Trustee",
-    description: "IISER Tirupati",
+    designation: "Associate Professor",
+    description: "Indian Institute of Science Education and Research, Tirupati",
     img: "../assets/img/national-faculty/Siva Vallabhapurapu ,  IISER Tirupati.png"
   },
   {
     name: "Soumen Basak",
-    designation: "Managing Trustee",
-    description: "NII",
+    designation: "Staff Scientist-VI",
+    description: "National Institute of Immunology (NII)",
     img: "../assets/img/national-faculty/Soumen Basak, NII.jpeg.png"
   },
   {
     name: "Subir Biswas",
-    designation: "Managing Trustee",
-    description: "ACTREC",
+    designation: "Principal Investigator",
+    description: "Advanced Centre for Treatment, Research and Education in Cancer(ACTREC)",
     img: "../assets/img/national-faculty/Subir Biswas ACTREC.png"
   },
   {
     name: "Sudha Kumari",
-    designation: "Managing Trustee",
-    description: "IISc",
+    designation: "Assistant Professor",
+    description: "Department of Microbiology and Cell Biology, IISc",
     img: "../assets/img/national-faculty/Dr. Sudha Kumari, IISc.png"
   },
   {
     name: "Suktilang Majaw",
-    designation: "Managing Trustee",
-    description: "NEHU Shillong",
+    designation: "Assistant Professor",
+    description: "Department of Biotechnology & Bioinformatics, NEHU Shillong",
     img: "../assets/img/national-faculty/Suktilang Majaw , NEHU Shillong.png"
   },
   {
     name: "Sumit Hira",
-    designation: "Managing Trustee",
-    description: "University of Burdwan",
+    designation: "Assistant Professor",
+    description: " Immunology, University of Burdwan",
     img: "../assets/img/national-faculty/Sumit Hira, Univ of Burdwan.png"
   },
   {
     name: "Susmita Jha",
-    designation: "Managing Trustee",
-    description: "IIT Jodhpur",
+    designation: "Associate Professor",
+    description: "Indian Institute of Technology (IIT) Jodhpur",
     img: "../assets/img/national-faculty/Susmita Jha , IIT Jodhpur.png"
   },
   {
     name: "Taruna Madan",
-    designation: "Managing Trustee",
-    description: "NIRRH",
+    designation: "Scientist F & Head",
+    description: "Department of Innate Immunity, NIRRH",
     img: "../assets/img/national-faculty/Dr. Taruna Madan, NIRRH.png"
   },
   {
     name: "Tina Mukherjee",
-    designation: "Managing Trustee",
-    description: "InStem",
+    designation: "Associate Professor",
+    description: "The Institute for Stem Cell Science and Regenerative Medicine (inStem)",
     img: "../assets/img/national-faculty/Tina Mukherjee ,  InStem.png"
   },
   {
     name: "Udipi Ramagopal",
-    designation: "Managing Trustee",
-    description: "GITAM",
+    designation: "Professor",
+    description: "Gandhi Institute of Technology and Management (GITAM)",
     img: "../assets/img/national-faculty/Udipi Ramagopal , GITAM.png"
   },
   {
     name: "Uma Kanga",
-    designation: "Managing Trustee",
-    description: "AIIMS Delhi",
+    designation: "Assistnat Professor",
+    description: "Department of Transplant Immunology and Immunogenetics, AIIMS, New Delhi",
     img: "../assets/img/national-faculty/Dr. Uma Kanga, AIIMS.png"
   },
   {
+    name: "Vani Janakiraman",
+    designation: "Professor",
+    description: "Department Of Biotechnology, Indian Institute of technology Madras",
+    img: "../assets/img/national-faculty/Vani Janakiraman, IIT Madras.png"
+  },
+  {
     name: "Veena S Patil",
-    designation: "Managing Trustee",
-    description: "NII",
+    designation: "Scientist",
+    description: "National Institute of Immunology (NII)",
     img: "../assets/img/national-faculty/Veena S Patil, NII.png"
   },
   {
     name: "Vineeta Shobha",
-    designation: "Managing Trustee",
-    description: "St. John's Medical College ",
+    designation: "Professor & Head",
+    description: "Department of Clinical Immunology and Rheumatology, St. John's Medical College",
     img: "../assets/img/national-faculty/Vineeta Shobha , St. John's Medical College.png"
-  },
+  }
 ];
 
 
