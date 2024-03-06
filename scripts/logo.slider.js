78d473247727dcc46249900ad7dee8472306e09e
rezwanhussain <rezwanhussain80@gmail.com>
2024-03-06 15:13:16 +0530

    Updated poster presentation and guidelines page in all the page header

--- scripts/logo.slider.js
+++ scripts/logo.slider.js
@@ -1,7 +1,7 @@
-const logoSlides = document.querySelector('.logo_slides');
+// const logoSlides = document.querySelector('.logo_slides');
 
-if(document.querySelector('.logo_slides')){
-    const DupliLogoSlides = logoSlides.cloneNode(true);
-    logoSlides.parentNode.appendChild(DupliLogoSlides)
-}
+// if(document.querySelector('.logo_slides')){
+//     const DupliLogoSlides = logoSlides.cloneNode(true);
+//     logoSlides.parentNode.appendChild(DupliLogoSlides)
+// }
 
